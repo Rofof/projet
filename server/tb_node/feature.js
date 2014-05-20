f70e6fe8d527fa0e62663e9d76add0e2e03f1559
Rofof <rodolphe.malaguti@gmail.com>
2014-05-21 00:02:32 +0200

    "ajoute serveur"

---
+++ server/tb_node/feature.js
@@ -0,0 +1,71 @@
+
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'oatic',
+  password : 'oatic',
+  database : 'bdd_tb_stub'
+});
+connection.connect(function(err) {
+  // connected! (unless `err` is set)
+  console.log("CONFIG connect callabck param ==> " + err);
+
+});
+
+var Feature_Get1 = function(req, res) {
+	console.log('Feature_Get1 [' + req.params.id + ']');
+	res.writeHead(200, {'Content-Type': 'application/javascript'});
+	  
+	connection.query('call getTable(\'' + req.params.id + '\');', function(err, rows) {
+		//console.log(err);
+
+		var result;
+		  
+		if (!err) {
+
+			result = { 'result' : 'success', 'data' : rows[0]};
+			
+		
+		}
+		else {
+			console.log("Query error ==> " + err);
+			result = { 'result' : 'failure'};
+		}
+
+		res.end(JSON.stringify(result));
+
+	});
+
+} // end of Feature_Get1
+
+var Feature_Post1 = function(req, res) {
+	console.log('Feature_Post1 [' + req.params.id + '] : ' + req.body);
+	res.writeHead(200, {'Content-Type': 'application/javascript'});
+	  
+	connection.query('call SetTable(\'' + req.params.id + '\',\'' + req.body.value + '\');', function(err, rows) {
+		//console.log(err);
+
+		var result; 
+		  
+		if (!err) {
+
+			result = { 'result' : 'success'};
+			
+		
+		}
+		else {
+			console.log("Query error ==> " + err);
+			result = { 'result' : 'failure'};
+		}
+		  
+		res.end(JSON.stringify(result));
+
+	});
+
+} // end of Feature_Post1
+
+
+
+exports.Feature_Get1 = Feature_Get1;
+exports.Feature_Post1 = Feature_Post1;
---
+++ server/tb_node/server.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+
+var feature = require('./feature.js');
+
+var app = express();
+
+app.configure(function() {
+  app.use(express.bodyParser({ uploadDir : '/var/www/tmp'}));
+  app.use(app.router);
+});
+
+http.createServer(app).listen('8111');
+
+
+app.all('*', function(req, res, next) {
+  //console.log(req);
+
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*');     
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', '*');
+  
+  if('OPTIONS' == req.method) return res.send(200);
+
+  next();
+});
+
+app.post('/platoatic/p1/:id', feature.Feature_Post1)
+  .get('/platoatic/g1/:id', feature.Feature_Get1);
